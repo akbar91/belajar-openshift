b527c60cbf0c02575eef6154e79b42386de5d4f6
akbarfachri <akbar.fachriansyah@gmail.com>
2015-06-29 21:28:53 +0700

    hallo servlet

---
+++ src/main/java/com/fachri/akbar/belajar/servlet/HalloServlet.java
@@ -0,0 +1,33 @@
+package com.fachri.akbar.belajar.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/halo")
+
+public class HalloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+										throws IOException, ServletException {
+		res.setContentType("text/html");
+
+		PrintWriter output = res.getWriter();
+
+		output.println("<!doctype html>");
+		output.println("<html lang=\"en\">");
+		output.println("<head><title>Hallo Servlet</title></head>");
+		output.println("<body>");
+		output.println("<h1>HaloServlet</h1>");
+		output.println("Waktu : "+new Date());
+		output.println("</body>");
+		output.println("</html>");
+
+		output.flush();
+
+	}
+}
